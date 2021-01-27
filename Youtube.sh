0ece288742fa6e219d222b9907716b8972fc79ec
Fadel <59105868+SrWither@users.noreply.github.com>
2021-01-26 23:42:26 -0300

    Update Youtube.sh

--- Youtube.sh
+++ Youtube.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-electron7 /home/fadel/desarrollos/Electron/YouTube.js
+youtube() {
+
+  while true; do
+    read -p "¿Que tipo de video te gusta mas? (Juegos/Música)" vid
+    case $vid in
+      Juegos ) echo "Genial, que buen gusto!";;
+      Musica ) echo "Me encanta la música!";;
+      * ) echo "coloca una opción valida"; youtube;;
+      esac
+  done
+  }
+  youtube
